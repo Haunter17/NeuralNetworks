ae76fed7bf06c6a8834f2b1bd7864a0a58e35390
Haunter17 <haunterhacker17@gmail.com>
2017-01-27 21:36:34 -0800

    p5

--- adaline.py
+++ adaline.py
@@ -25,15 +25,16 @@ def normalize(vec):
 
 def MSE(weights, samples):
     accum = 0
+    m = len(samples)
     for sample in samples:
         inputSamp = [1] + sample[0]
         desired = sample[1]
         output = inner(weights, inputSamp)
         diff = desired - output
         accum += (diff ** 2)
-    return accum
+    return accum * 1.0 / m
 
-def train(samples, limit, verbose=False, freq=100, norm=True, rate=0.1, regression=False, epsilon=1):
+def train(samples, limit, verbose=False, freq=100, norm=True, rate=0.1, regression=False, epsilon=1.0):
     """ Train a perceptron with the set of samples. """
     """ samples is a list of pairs of the form [input vector, desired output] """
     """ limit is a limit on the number of epochs """
@@ -85,6 +86,7 @@ def train(samples, limit, verbose=False, freq=100, norm=True, rate=0.1, regressi
             if  verbose and epoch % freq == 0:
                 print "epoch {} : MSE is {}".format(epoch, round(newMSE, 3))
             if newMSE < epsilon:
+                print "epoch {} : MSE is {}".format(epoch, round(newMSE, 3))
                 break
 
     print 'number of epochs: {}, final weights = {}'.format(epoch - 1, [round(x, 3) for x in weights])
@@ -104,8 +106,8 @@ def test(samples, weights, verbose=False, norm=True, regression=False):
             error = desired - output
             if verbose:
                 print 'input = {} desired = {} output = {}'\
-                    .format([round(x, 3) for x in inputSamp], desired, output)
-        print "MSE: {}".format(mse)
+                    .format([round(x, 3) for x in inputSamp], desired, round(output, 3))
+        print "MSE: {}".format(round(mse, 3))
         return mse
     else:
         wrong = 0
@@ -129,18 +131,26 @@ implies = [[[0, 0], 1], [[0, 1], 1], [[1, 0], 0], [[1, 1], 1]]
 nandSamples = [[[0, 0], 1], [[0, 1], 1], [[1, 0], 1], [[1, 1], 0]]
 iffSamples = [[[0, 0], 1], [[0, 1], 0], [[1, 0], 0], [[1, 1], 1]]
 
-widrowHoffTrain = [[[1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1], 60], [[1,1,1,-1,1,-1,-1,-1,1,1,1,-1,1,1,1,-1], 0], \
-[[1,1,1,-1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1], -60]]
-widrowHoffTest = [[[-1,1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1], 60], [[-1,1,1,1,-1,1,-1,-1,-1,1,1,1,-1,1,1,1], 0], \
-[[-1,1,1,1,-1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1], -60]]
+widrowHoffTrain = [[[1,1,1,0,0,1,0,0,0,1,0,0,0,1,0,0], 60], [[1,1,1,0,1,0,0,0,1,1,1,0,1,1,1,0], 0], \
+[[1,1,1,0,1,1,0,0,1,0,0,0,1,0,0,0], -60]]
+widrowHoffTest = [[[0,1,1,1,0,0,1,0,0,0,1,0,0,0,1,0], 60], [[0,1,1,1,0,1,0,0,0,1,1,1,0,1,1,1], 0], \
+[[0,1,1,1,0,1,1,0,0,1,0,0,0,1,0,0], -60]]
 
+hvTest = [[[1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0], -10], [[0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0], -10], \
+[[0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0], -10], [[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1], -10], \
+[[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0], 10],[[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0], 10], \
+[[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0], 10], [[0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1], 10]]
+import random
+random.shuffle(hvTest)
+hvTrain = hvTest[:-1]
 # weights = train(iffSamples, 500, verbose=True, norm=False, rate=0.001)
-# weights = train(cancer.cancertrainingSamples, 173, verbose=False, norm=False, rate=0.01)
+# weights = train(cancer.cancertrainingSamples, 173, verbose=False, norm=False, rate=0.01)[2]
 # test(cancer.cancertrainingSamples, weights, verbose=False, norm=False)
 # test(cancer.cancertestSamples, weights, verbose=False, norm=False)
 
-weights = train(widrowHoffTrain, 10000, verbose=True, freq=500, norm=False, \
-    rate=0.001, regression=True, epsilon=10)[2]
-test(widrowHoffTest, weights, verbose=True, norm=False, regression=True)
+# weights = train(widrowHoffTrain + widrowHoffTest, 200, verbose=True, freq=50, norm=False, \
+#     rate=0.03, regression=True, epsilon=1.0)[2]
+# test(widrowHoffTrain + widrowHoffTest, weights, verbose=True, norm=False, regression=True)
 
-
+weights = train(hvTrain, 3000, verbose=False, freq=1000, norm=False, rate=0.002, regression=True, epsilon=1)[2]
+test(hvTest, weights, verbose=True, norm=False, regression=True)
