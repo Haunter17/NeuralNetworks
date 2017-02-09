65aaa3cfb09044aa868f33ece208091c80e41bb6
Haunter17 <haunterhacker17@gmail.com>
2017-02-09 01:30:51 -0800

    backprop first ver

--- rtrl.py
+++ rtrl.py
@@ -1,5 +1,6 @@
 #RTRL Learning by Zhepei Wang
 import numpy as np
+import copy
 def parseIO(X):
     X = np.matrix(X).transpose()
     return X
@@ -20,20 +21,54 @@ inputDim = X.shape[0]
 numSample = X.shape[1]
 outputDim = y.shape[1]
 hiddenDim = outputDim
-
-
 X = np.hstack((X, np.matrix([[0] for i in range(inputDim)])))
-
 W = initW(hiddenDim, inputDim + hiddenDim)
-H = np.matrix([[0. for col in range(numSample + 1)] for row in range(hiddenDim)])
-Z = np.vstack((X, H))
 
-def forwardFeed(t):
-    # H = np.matrix([[0. for col in range(numSample + 1)] for row in range(hiddenDim)])
-    # Z = np.vstack((X, H))
-    # for t in range(1, numSample + 1):
-    #     Z[inputDim:, t] = sigmoid(W * Z[:, t - 1])
-    # return Z[inputDim:, 1:]
+def forwardFeed(Z, t):
+    # Z supposed to be updated
     Z[inputDim:, t] = sigmoid(W * Z[:, t - 1])
     return Z[inputDim: , t]
 
+def train(X, y, rate=0.4, max_iter=100, threshold=1.0, displayInterval=5, noisy=False):
+    epoch = 0
+    global W
+    for epoch in range(max_iter):
+        H = np.matrix([[0. for col in range(numSample + 1)] for row in range(hiddenDim)])
+        Z = np.vstack((X, H))
+        MSE = 0
+        gradWT = [[[0. for col in range(inputDim + hiddenDim)] \
+        for row in range(hiddenDim)] for t in range(numSample + 1)]
+        gradW = copy.deepcopy(gradWT[0])
+
+        pTL = [[[[0. for col in range(inputDim + hiddenDim)] \
+        for row in range(hiddenDim)] \
+        for k in range(hiddenDim)] for t in range(numSample + 1)]
+        
+        for t in range(1, numSample + 1):
+            pred = forwardFeed(Z, t)
+            e = y[:, t - 1] - pred
+            MSE += 0.5 * ((e.transpose() * e)).item(0)
+            # calculate p and gradW for sample T
+            for i in range(hiddenDim):
+                for j in range(inputDim + hiddenDim):
+                    accumK = 0
+                    for k in range(hiddenDim):
+                        accumL = 0
+                        for l in range(hiddenDim):
+                            accumL += W.item(k, inputDim + l) * pTL[t - 1][l][i][j]
+                        if i == k:
+                            accumL += Z.item(j, t - 1)
+                        pTL[t][k][i][j] = Z.item(inputDim + k, t) * (1. - Z.item(inputDim + k, t)) \
+                        * accumL
+                        accumK += e.item(k) * pTL[t][k][i][j]
+                    gradWT[t][i][j] += accumK
+        for t in range(len(gradWT)):
+            for i in range(hiddenDim):
+                for j in range(inputDim + hiddenDim):
+                    gradW[i][j] = gradWT[t][i][j]
+        if epoch % displayInterval == 0:
+            print "MSE for epoch {}: {}".format(epoch, MSE)
+        gradW = np.matrix(gradW)
+        W = W + rate * gradW
+
+train(X, y)
