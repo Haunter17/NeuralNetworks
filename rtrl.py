e0cd66640f2645f3d9b531f75debfb91fe222024
Haunter17 <haunterhacker17@gmail.com>
2017-02-10 16:49:56 -0800

    better output

--- rtrl.py
+++ rtrl.py
@@ -53,6 +53,15 @@ def genBeats(pattern, numSlots):
     X[0] = 1
     return X, y
 
+def decomposePower2(x):
+    powers = []
+    i = 1
+    while i <= x:
+        if i & x:
+            powers.append(int(np.log2(i)))
+        i <<= 1
+    return powers
+
 # global config
 X = []
 y = []
@@ -75,6 +84,7 @@ def train(X, y, neuronType, rate=1.0, max_iter=25000, threshold=0.05, \
     displayInterval=100, noisy=False):
     epoch = 0
     global W
+    numTrack = int(np.log2(outputDim))
     for epoch in range(max_iter):
         if epoch % displayInterval == 0:
             print "Epoch {}".format(epoch)
@@ -92,10 +102,18 @@ def train(X, y, neuronType, rate=1.0, max_iter=25000, threshold=0.05, \
             slotMSE = 0.5 * ((e.transpose() * e)).item(0)
             MSE += slotMSE
             if epoch % displayInterval == 0:
-                print "slot {} click {} desired {} predicted {} \
-                MSE {}".format(t - 1, X.item(t - 1), \
-                    [round(p) for p in np.asarray(desired.transpose())[0]], \
-                    [round(p) for p in np.asarray(pred.transpose())[0]], round(slotMSE, 3))
+                print "slot: {} click: {} MSE: {}".format(t - 1, X.item(t - 1), round(slotMSE, 3))
+                if noisy:
+                    print "desired {} predicted {} \
+                    MSE {}".format(t - 1, X.item(t - 1), \
+                        [round(p) for p in np.asarray(desired.transpose())[0]], \
+                        [round(p) for p in np.asarray(pred.transpose())[0]])
+                else:
+                    desireTrack = decomposePower2(np.argmax(np.asarray(desired.transpose())[0]))
+                    predTrack = decomposePower2(np.argmax(np.asarray(pred.transpose())[0]))
+                    for trackIndex in range(numTrack):
+                        print "track {} desired {} predicted {}".format(trackIndex, \
+                            1 if trackIndex in desireTrack else 0, 1 if trackIndex in predTrack else 0)
             # calculate p and gradW for sample T
             for i in range(hiddenDim):
                 for j in range(inputDim + hiddenDim):
@@ -122,11 +140,29 @@ def train(X, y, neuronType, rate=1.0, max_iter=25000, threshold=0.05, \
 def assess(W, X, y, neuronType):
     H = np.matrix([[0. for col in range(numSample + 1)] for row in range(hiddenDim)])
     Z = np.vstack((X, H))
+    numTrack = int(np.log2(outputDim))
     print '---------testing----------'
+    MSE = 0
     for t in range(1, numSample + 1):
         Z[inputDim:, t] = neuronType.fun(W * Z[:, t - 1])
-        print "predicted: {}; expected: {}".format(np.argmax(Z[inputDim:, t]), np.argmax(y[:, t - 1]))
-    return Z[inputDim:, -1]
+        pred = Z[inputDim:, t]
+        desired = y[:, t - 1]
+        e = desired - pred
+        slotMSE = 0.5 * ((e.transpose() * e)).item(0)
+        MSE += slotMSE
+        print "slot: {} click: {} MSE: {}".format(t - 1, X.item(t - 1), slotMSE)
+        # print "predicted: {}; expected: {}".format(np.argmax(Z[inputDim:, t]), np.argmax(y[:, t - 1]))
+        desireTrack = decomposePower2(np.argmax(np.asarray(desired.transpose())[0]))
+        predTrack = decomposePower2(np.argmax(np.asarray(pred.transpose())[0]))
+        for trackIndex in range(numTrack):
+            trackOnDesired = trackIndex in desireTrack
+            trackOnPred = trackIndex in predTrack
+            print "track {} desired {} predicted {} {} prediction".format(trackIndex, \
+                1 if trackOnDesired else 0, 1 if trackOnPred else 0, \
+                "correct" if (trackOnPred and trackOnDesired) or (not trackOnPred and not trackOnDesired) else "wrong")
+    MSE /= numSample
+    print "Final MSE: {}".format(round(MSE, 4))
+    return MSE
 
 def drumMachine(pattern, numSlots, runLength=25000, rate=0.5, neuronType=logsig):
     global X, y, inputDim, numSample, outputDim, hiddenDim, W
@@ -144,9 +180,11 @@ def drumMachine(pattern, numSlots, runLength=25000, rate=0.5, neuronType=logsig)
     Wopt = train(X, y, neuronType, rate=rate, max_iter=runLength)
     assess(Wopt, X, y, neuronType)
 
-# pattern = [[0,4],[2,5,6], [0, 2, 4, 6]]
-pattern = [[7], [0,1,2,3,4,5,6,8,9,10,11,12,13,14], [4, 12], [0, 8, 10]]
+pattern = [[0,4],[2,5,6], [0, 2, 4, 6]]
+# pattern = [[7], [0,1,2,3,4,5,6,8,9,10,11,12,13,14], [4, 12], [0, 8, 10]]
+
 def main():
-    drumMachine(pattern, 16, runLength=1000, rate=1.0)
+    drumMachine(pattern, 8, runLength=10000, rate=0.8)
+    # print decomposePower2(5)
 
 main()
