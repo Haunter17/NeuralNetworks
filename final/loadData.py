3b14a77c8fd3f6054b25d899e0aba382f690aa80
Haunter17 <haunterhacker17@gmail.com>
2017-04-15 03:03:41 -0700

    Fixing wrong data

--- final/loadData.py
+++ final/loadData.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 # file configurations
-filePath = 'kdd_small.csv'
+filePath = 'kdd_10.csv'
+# 'num_root', 'num_file_creations', 'num_shells' are with mixed types
 features = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', \
 'dst_bytes', 'land', 'wrong_fragment', 'urgent', 'hot', 'num_failed_logins', \
 'logged_in', 'num_compromised', 'root_shell', 'su_attempted', 'num_root', \
@@ -12,8 +13,6 @@ features = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', \
 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate', \
 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate', 'dst_host_srv_serror_rate', \
 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate', 'label']
-df = pd.read_csv(filePath, names = features)
-df = df.sample(frac = 1).reset_index(drop = True) # shuffle rows
 continuous_features = ['duration', 'src_bytes', 'dst_bytes', 'wrong_fragment', \
 'urgent', 'hot', 'num_failed_logins', 'num_compromised', 'root_shell', 'su_attempted', \
 'num_root', 'num_file_creations', 'num_shells', 'num_access_files', 'num_outbound_cmds', \
@@ -24,20 +23,28 @@ continuous_features = ['duration', 'src_bytes', 'dst_bytes', 'wrong_fragment', \
 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate']
 category_features = ['protocol_type', 'service', 'flag', 'land', 'logged_in', \
 'is_host_login', 'is_guest_login']
-# hash categorical features into floats
+df = pd.read_csv(filePath, names = features, low_memory = False)
+
+# convert a few mixed continuous features into integers
+for feature in ['num_root', 'num_file_creations', 'num_shells']:
+	df[feature] = df[feature].astype('category')
+	df[feature] = getattr(df, feature).cat.codes
+	df[feature] = df[feature].astype('int64')
+# hash categorical features into integers
 for category in category_features:
 	df[category] = df[category].astype('category')
 	df[category] = getattr(df, category).cat.codes
 # hash labels
 df['label'] = df['label'].astype('category')
-df['label'] = df.label.cat.codes
+df['label'] = df.label.cat.codes # 11 stands for normal
+
 # partition data
+df = df.sample(frac = 1).reset_index(drop = True) # shuffle rows
 df['type'] = ''
 df.loc[0: int(0.75 * len(df)) - 1, 'type'] = 'train'
 df.loc[int(0.75 * len(df)): , 'type'] = 'test'
-# # convert data into matrix
+# convert data into matrix
 X_train = df[df.type == 'train'][[col for col in df.columns if col not in ['label', 'type']]]
-y_train = df[df.type == 'train'].label
-
+y_train = df[df.type == 'train']['label']
 X_test = df[df.type == 'test'][[col for col in df.columns if col not in ['label', 'type']]]
-y_test = df[df.type == 'test'].label
+y_test = df[df.type == 'test']['label']
